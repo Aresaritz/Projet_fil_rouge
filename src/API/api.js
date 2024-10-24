c991370f1f2467059ea22579768351325b127d47
Baptiste <baptiste.blasquez@gmail.com>
2024-10-24 08:20:30 +0200

    Started API

---
+++ src/API/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+// Récuperer la listes des taches
+app.get('/tasks', (req, res) => {
+  res.send([
+    { task1: 'Démarrer projet' },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
