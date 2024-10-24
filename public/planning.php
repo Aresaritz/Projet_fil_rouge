8ea21d673cc308d384b81a97fba468ab53b4ea2b
Baptiste <baptiste.blasquez@gmail.com>
2024-10-24 12:42:39 +0200

    Can now create a task

--- public/planning.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../src/php/functions.php';
-require_once __DIR__ . '/../src/php/db.php';
-
-session_start();
-if (!isset($_SESSION['id'])) {
-  header('Location: index.php');
-  exit;
-}
